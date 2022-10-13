d2dbcd27a4f308282dda023e5e5202114f1da7bd
ValerioScharizza <valerio.schiarizza@live.it>
2022-10-13 15:03:12 +0200

    Snack 2 done

--- index.php
+++ index.php
@@ -26,49 +26,73 @@ e un array contenente i suoi voti scolastici. Stampare Nome, Cognome e la media 
 
 
 
-<!-- SNACK 1
+ <!-- SNACK 1
 
 Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. 
 Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra 
 di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60-->
 
-<?php
+ <?php
+    $matches = [
+        [
+            'home' => 'New York Hamburgers',
+            'guest' => 'Ohio Fries Squad',
+            'home-points' => 55,
+            'guest-points' => 60,
+        ],
+        [
+            'home' => 'Orlando Bloom\'s Cheerleaders',
+            'guest' => 'Avana De Cuba Drinkers',
+            'home-points' => 60,
+            'guest-points' => 48,
+        ],
+        [
+            'home' => 'WDC Rolling Stonks',
+            'guest' => 'Magpies',
+            'home-points' => 52,
+            'guest-points' => 60,
+        ],
+    ];
 
-$matches = [
-    [
-        'home' => 'New York Hamburgers',
-        'guest' => 'Ohio Fries Squad',
-        'home-points' => 55,
-        'guest-points' => 60,
-    ],
-    [
-        'home' => 'Orlando Bloom\'s Cheerleaders',
-        'guest' => 'Avana De Cuba Drinkers',
-        'home-points' => 60,
-        'guest-points' => 48,
-    ],
-    [
-        'home' => 'WDC Rolling Stonks',
-        'guest' => 'Magpies',
-        'home-points' => 52,
-        'guest-points' => 60,
-    ],
-];
+    echo '<h2 style="color: red">SNACK 1</h2>';
 
-echo '<h2 style="color: red">SNACK 1</h2>';
+    for ($i = 0; $i < count($matches); $i++) {
 
-for ($i = 0; $i < count($matches); $i++) {
+    ?>
 
-?>
+     <div style="padding-bottom: 10px; color: darkgreen">
+         <?= $matches[$i]['home'] . ' - ' . $matches[$i]['guest'] . ' | ' . $matches[$i]['home-points'] . '-' . $matches[$i]['guest-points'] ?>
+     </div>
 
-<div style="padding-bottom: 10px; color: darkgreen">
+ <?php
 
-    <?= $matches[$i]['home'] . ' - ' . $matches[$i]['guest'] . ' | ' . $matches[$i]['home-points'] . '-' . $matches[$i]['guest-points'] ?>
-</div>
+    }
+    ?>
 
-<?php
 
-}
+ <!-- SNACK 2
 
-?>
+Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) 
+che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. 
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+
+ <?php
+    echo '<h2 style="color: red">SNACK 2</h2>';
+
+    $name = $_GET['name'];
+    $mail = $_GET['mail'];
+    $age = $_GET['age'];
+
+    $verification = '';
+
+    if (trim(strlen($name)) > 3 && strpos($mail, '@') && strpos($mail, '.') && is_numeric($age)) {
+        $verification = 'Accesso riuscito';
+    } else {
+        $verification = 'Accesso negato';
+    }
+    ?>
+
+ <div style="font-size: 1.5rem">
+     <?php echo $verification ?>
+ </div>
